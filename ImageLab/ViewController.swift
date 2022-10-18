42f617695bb3b133af91a05c0805a07ee0da775b
Eric Larson <eclarson@smu.edu>
2022-10-18 09:20:07 -0500

    cleaning up code comments

--- ImageLab/ViewController.swift
+++ ImageLab/ViewController.swift
@@ -19,13 +19,21 @@ class ViewController: UIViewController   {
         super.viewDidLoad()
         
         self.view.backgroundColor = nil
+        
+        // the remainder of this example should probably be in
+        // a model somewhere, rather than in the VC (but just a quick example)
+        
+        // create array of filters
         self.setupFilters()
         
+        // setup video manager with view to render and camera to use
         self.videoManager = VideoAnalgesic(mainView: self.view)
         self.videoManager.setCameraPosition(position: .front)
        
+        // what function to call between capture and render
         self.videoManager.setProcessingBlock(newProcessBlock: self.processImage)
         
+        // start processing!
         if !videoManager.isRunning{
             videoManager.start()
         }
@@ -37,9 +45,10 @@ class ViewController: UIViewController   {
         filters[hueIndex].setValue(sender.value, forKey: "inputAngle")
     }
     
-    //MARK: Setup filtering
+    //MARK: Setup and Apply Filtering Array
     func setupFilters(){
         filters = []
+        
         let filterBloom = CIFilter(name: "CIBloom")!
         filterBloom.setValue(0.5, forKey: kCIInputIntensityKey)
         filterBloom.setValue(20, forKey: "inputRadius")
@@ -52,6 +61,7 @@ class ViewController: UIViewController   {
     }
     
     func applyFilters(inputImage:CIImage)->CIImage{
+        // basically the same as last time
         var retImage = inputImage
         for filt in filters{
             filt.setValue(retImage, forKey: kCIInputImageKey)
@@ -62,8 +72,8 @@ class ViewController: UIViewController   {
     
     //MARK: Process image output
     func processImage(inputImage:CIImage) -> CIImage{
-        //return inputImage
-        return applyFilters(inputImage: inputImage)
+        return inputImage
+        //return applyFilters(inputImage: inputImage)
     }
 
 }
