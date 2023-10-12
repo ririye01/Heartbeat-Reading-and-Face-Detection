88f5de4ba9c68fa5ed84b06b6520e981109d8d04
Eric Larson <eclarson@smu.edu>
2023-10-12 09:12:53 -0500

    Fixed some issues with the Video Pixel frame buffering in an MTKView

--- ImageLab/ViewController.swift
+++ ImageLab/ViewController.swift
@@ -7,87 +7,43 @@
 //
 
 import UIKit
-import AVFoundation
+import MetalKit
 
 class ViewController: UIViewController   {
 
-    //MARK: Class Properties
-    var filters : [CIFilter]! = nil
-    var videoManager:VideoAnalgesic! = nil
-    let pinchFilterIndex = 1
+    
+    @IBOutlet weak var camerView: MTKView!
+    var videoModel:VideoModel?
     
     //MARK: ViewController Hierarchy
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = nil
-        self.setupFilters()
-        
-        self.videoManager = VideoAnalgesic(mainView: self.view)
-        self.videoManager.setCameraPosition(position: .front)
-        
-        self.videoManager.setProcessingBlock(newProcessBlock: self.processImage)
-        
-        if !videoManager.isRunning{
-            videoManager.start()
-        }
+        videoModel = VideoModel(view: self.camerView)
     
     }
     
-    //MARK: Setup filtering
-    func setupFilters(){
-        filters = []
-        
-        // add bloom filter with new radius
-        let filterBloom = CIFilter(name: "CIBloom")!
-        filterBloom.setValue(0.5, forKey: kCIInputIntensityKey)
-        filterBloom.setValue(20, forKey: "inputRadius")
-        filters.append(filterBloom)
-        
-        // add pinch filter
-        let filterPinch = CIFilter(name:"CIBumpDistortion")!
-        filterPinch.setValue(-0.5, forKey: "inputScale")
-        filterPinch.setValue(75, forKey: "inputRadius")
-        filterPinch.setValue(CIVector(x:self.view.bounds.size.height-50,y:self.view.bounds.size.width), forKey: "inputCenter")
-        filters.append(filterPinch)
-        
-    }
     
-    func applyFilters(inputImage:CIImage)->CIImage{
-        var retImage = inputImage
-        for filt in filters{
-            filt.setValue(retImage, forKey: kCIInputImageKey)
-            retImage = filt.outputImage!
-        }
-        return retImage
-    }
-    
-    //MARK: Process image output
-    func processImage(inputImage:CIImage) -> CIImage{
-        return applyFilters(inputImage: inputImage)
-    }
 
     @IBAction func panRecognized(_ sender: UIPanGestureRecognizer) {
         
-        let uiPoint = sender.location(in: self.view)
+        let uiPoint = sender.location(in: self.camerView)
+        
+        videoModel?.setFilterLocation(point: uiPoint)
         
         // this must be custom for each camera position and for each orientation
         // CoreImage has origin in lower left of landscape
         // UIKit has origin in upper left in portrait
         // also, if applying "flipped" or rotations with VideoANalgesic, that must be accounted for
-        let tmp = CIVector(x:uiPoint.y,
-                           y:uiPoint.x)
-        self.filters[pinchFilterIndex].setValue(tmp, forKey: "inputCenter")
+        
     }
     
     @IBAction func tapRecognized(_ sender: UITapGestureRecognizer) {
-        let uiPoint = sender.location(in: self.view)
+        let uiPoint = sender.location(in: self.camerView)
         
         // this must be custom for each camera position and for each orientation
-        let tmp = CIVector(x:uiPoint.y,
-                           y:uiPoint.x)
         
-        self.filters[pinchFilterIndex].setValue(tmp, forKey: "inputCenter")
+        videoModel?.setFilterLocation(point: uiPoint)
     }
     
 }
