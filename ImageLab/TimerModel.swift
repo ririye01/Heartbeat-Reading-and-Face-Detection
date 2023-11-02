fc6f9a7757c99a43d4e6def8fcac338b2fd871c6
Reece Iriye <63213726+ririye01@users.noreply.github.com>
2023-11-02 01:56:57 -0500

    Update TimerModel.swift

--- ImageLab/TimerModel.swift
+++ ImageLab/TimerModel.swift
@@ -10,28 +10,33 @@ import Foundation
 import UIKit
 
 class TimerModel: NSObject {
+    // Maintain variables for storing the remaining time left in the timer and the time display
     private var remainingTime: TimeInterval = 0
     var timeDisplay: String = "30:00"
     
-    
+    // Control logic for changing the display as the timer clicks down
     func changeDisplay(){
         let time = NSInteger(remainingTime)
         let seconds = time/100
         let millisecond = time%100
-        
+
+        // Ensure that time display is in the format of seconds:milliseconds for readability
         timeDisplay = String(format:"%0.2d:%0.2d", seconds, millisecond)
     }
-    
+
+    // Decrease time by 5 milliseconds to ensure updates aren't too frequent
     func decrementRemainingTime() {
         remainingTime -= 5
     }
-    
+
+    // Allow parameter to be fed in through VC logic to call this function and adjust the timer
+    // model's remaining time left
     func setRemainingTime(withInterval interval: TimeInterval){
         remainingTime = interval
     }
-    
+
+    // Retrieve the remaining time left as a TimeInterval type
     func getRemainingTime() -> TimeInterval {
         return remainingTime
     }
-    
 }
